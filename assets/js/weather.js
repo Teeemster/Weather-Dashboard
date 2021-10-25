1450e7d9340f5985752e4ab6e47273249b50c21f
Teeemster <jonathan_beach@ymail.com>
2021-10-24 18:07:29 -0700

    local storage fix

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,6 +1,5 @@
 //Form Variables
 var searchButton = document.getElementById("searchButton");
-var priorCity = document.getElementsByClassName("priorCity");
 var priorCityContainer = document.getElementById("priorCityContainer");
 var cityTextArea = document.getElementById("cityTextArea");
 var currentDayForecast = document.getElementById("currentDayForecast");
@@ -14,15 +13,44 @@ var apiKey = "&appid=e7ddda053e014bb0464681d224273dcf";
 var currRequestURL = "https://api.openweathermap.org/data/2.5/weather?q="
 var uvRequestURL = "https://api.openweathermap.org/data/2.5/onecall?"
 
+
+//Pull Prior Cities
+var cityArray = JSON.parse(localStorage.getItem('Cities')) || [];
+for (let i = 0; i < cityArray.length; i++) {
+    var priorCityButton = document.createElement("button");
+    priorCityButton.className = "priorCity col - 12";
+    priorCityButton.textContent = cityArray[i];
+    priorCityContainer.appendChild(priorCityButton)
+}
+
 //City Search Function and API Pull Function
 searchButton.addEventListener("click", function () {
+
+    //Clear Prior Data
+    currDayList.textContent = ""
+    fiveDayList.textContent = ""
+
     //City Search and Prior History Append
     var cityTextContent = cityTextArea.value;
     var listItemEl = document.createElement("button");
-    listItemEl.className = "priorCity col-8";
-    listItemEl.id = cityTextContent;
+    listItemEl.className = "priorCity col-12";
     listItemEl.textContent = cityTextContent;
     priorCityContainer.appendChild(listItemEl);
+
+    //Store Cities
+    var newData = cityTextContent
+
+
+    if (localStorage.getItem("Cities") == null) {
+        localStorage.setItem("Cities", "[]");
+    }
+
+    var oldData = JSON.parse(localStorage.getItem("Cities"))
+
+    oldData.push(newData)
+
+    localStorage.setItem("Cities", JSON.stringify(oldData))
+
 
     //API Current URL
     var currFullRequestURL = currRequestURL + cityTextContent + apiKey
@@ -41,31 +69,60 @@ searchButton.addEventListener("click", function () {
             //Temp
             for (var i = 0; i < 40; i = i + 8) {
                 var dateInfo = data.list[i].dt_txt;
-                console.log(dateInfo);
                 var tempInfo = data.list[i].main.temp;
                 tempInfo = tempInfo - 273.15;
                 tempInfo = tempInfo * 1.8 + 32;
                 tempInfo = Math.round(tempInfo);
                 var windInfo = data.list[i].wind.speed;
                 var humidityInfo = data.list[i].main.humidity;
-                var weatherInfo = "Date: " + dateInfo + " | Temp: " + tempInfo + " °F " + " | Wind: " + windInfo + " MPH " + " | Humidity: " + humidityInfo + " %";
-                var tempItem = document.createElement("li");
-                tempItem.textContent = weatherInfo;
-                fiveDayList.appendChild(tempItem);
+                var cloudInfo = data.list[i].weather[0].main;
+                console.log(cloudInfo);
+
+                if (cloudInfo == "Thunderstorm") {
+
+                    var weatherInfo = " 🌩 " + "  Date: " + dateInfo + " | Temp: " + tempInfo + " °F " + " | Wind: " + windInfo + " MPH " + " | Humidity: " + humidityInfo + " %";
+                    var tempItem = document.createElement("li");
+                    tempItem.textContent = weatherInfo;
+                    fiveDayList.appendChild(tempItem);
+
+                }
+
+                else if (cloudInfo == "Clouds") {
+
+                    var weatherInfo = " ⛅ " + "  Date: " + dateInfo + " | Temp: " + tempInfo + " °F " + " | Wind: " + windInfo + " MPH " + " | Humidity: " + humidityInfo + " %";
+                    var tempItem = document.createElement("li");
+                    tempItem.textContent = weatherInfo;
+                    fiveDayList.appendChild(tempItem);
+                }
+
+                else if (cloudInfo == "Mist" || cloudInfo == "Rain") {
+
+                    var weatherInfo = " 🌧 " + "  Date: " + dateInfo + " | Temp: " + tempInfo + " °F " + " | Wind: " + windInfo + " MPH " + " | Humidity: " + humidityInfo + " %";
+                    var tempItem = document.createElement("li");
+                    tempItem.textContent = weatherInfo;
+                    fiveDayList.appendChild(tempItem);
+                }
+
+                else {
+
+                    var weatherInfo = " 🌞 " + "  Date: " + dateInfo + " | Temp: " + tempInfo + " °F " + " | Wind: " + windInfo + " MPH " + " | Humidity: " + humidityInfo + " %";
+                    var tempItem = document.createElement("li");
+                    tempItem.textContent = weatherInfo;
+                    fiveDayList.appendChild(tempItem);
+
+                }
+
             }
+        });
 
-            //Store Five Day Forecast
-            localStorage.setItem(cityTextContent, JSON.stringify(weatherInfo));
-
-        })
-
-    //API Current URL
+    //API Current Fetch
     fetch(currFullRequestURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data)
+            var currentDate = new Date(data.dt * 1000);
+            var cityName = data.name;
             var currTempInfo = data.main.temp;
             var currWindInfo = data.wind.speed;
             var currHumidityInfo = data.main.humidity;
@@ -76,14 +133,46 @@ searchButton.addEventListener("click", function () {
             currTempInfo = currTempInfo - 273.15;
             currTempInfo = currTempInfo * 1.8 + 32;
             currTempInfo = Math.round(currTempInfo);
-            var currWeatherInfo = "Temp: " + currTempInfo + " °F " + " | Wind: " + currWindInfo + " MPH " + " | Humidity: " + currHumidityInfo + " %";
-            var currTempItem = document.createElement("li");
-            currTempItem.textContent = currWeatherInfo;
-            currDayList.appendChild(currTempItem);
+            var cloudCurrent = data.weather[0].main;
+            console.log(cloudCurrent);
 
-            //Store Current Day Weather
-            var currWeatherStore = cityTextContent + " current"
-            localStorage.setItem(currWeatherStore, JSON.stringify(currWeatherInfo))
+            //Cloud Icon
+            if (cloudCurrent == "Thunderstorm") {
+
+                var currWeatherInfo = " 🌩 " + cityName + " | " + currentDate + " | Temp: " + currTempInfo + " °F " + " | Wind: " + currWindInfo + " MPH " + " | Humidity: " + currHumidityInfo + " %";
+                var currTempItem = document.createElement("li");
+                currTempItem.textContent = currWeatherInfo;
+                currDayList.appendChild(currTempItem);
+
+            }
+
+            else if (cloudCurrent == "Clouds") {
+
+                var currWeatherInfo = " ⛅ " + cityName + " | " + currentDate + " | Temp: " + currTempInfo + " °F " + " | Wind: " + currWindInfo + " MPH " + " | Humidity: " + currHumidityInfo + " %";
+                var currTempItem = document.createElement("li");
+                currTempItem.textContent = currWeatherInfo;
+                currDayList.appendChild(currTempItem);
+
+            }
+
+            else if (cloudCurrent == "Mist" || cloudCurrent == "Rain") {
+
+                var currWeatherInfo = " 🌧 " + cityName + " | " + currentDate + " | Temp: " + currTempInfo + " °F " + " | Wind: " + currWindInfo + " MPH " + " | Humidity: " + currHumidityInfo + " %";
+                var currTempItem = document.createElement("li");
+                currTempItem.textContent = currWeatherInfo;
+                currDayList.appendChild(currTempItem);
+
+            }
+
+            else {
+
+                var currWeatherInfo = " 🌞 " + cityName + " | " + currentDate + " | Temp: " + currTempInfo + " °F " + " | Wind: " + currWindInfo + " MPH " + " | Humidity: " + currHumidityInfo + " %";
+                var currTempItem = document.createElement("li");
+                currTempItem.textContent = currWeatherInfo;
+                currDayList.appendChild(currTempItem);
+
+            }
+
 
             //UV Index Pull
             fetch(fullUVRequestURL)
@@ -92,26 +181,179 @@ searchButton.addEventListener("click", function () {
                 })
                 .then(function (data) {
                     var currUVItem = document.createElement("li");
-                    currUVItem.textContent = "UV Index: " + data.current.uvi;
-                    currDayList.appendChild(currUVItem);
+                    if (data.current.uvi <= 2) {
+                        currUVItem.textContent = "UV Index: " + data.current.uvi + " 🟢​ "
+                    }
 
-                    //Store Current UV
-                    var currUVStore = cityTextContent + " current UV"
-                    localStorage.setItem(currUVStore, JSON.stringify(data.current.uvi))
+                    else if (data.current.uvi <= 5) {
+                        currUVItem.textContent = "UV Index: " + data.current.uvi + " 🟡​ "
+                    }
+
+                    else {
+                        currUVItem.textContent = "UV Index: " + data.current.uvi + " ​🔴​ "
+                    }
+                    currDayList.appendChild(currUVItem);
+                })
+        })
+    
+});
+
+//Pull Prior City Button
+var priorButton = document.getElementsByClassName("priorCity");
+
+for (let i = 0; i < priorButton.length; i++) {
+    priorButton[i].addEventListener('click', priorFunction, false);
+}
+
+//City Search Function and API Pull Function
+function priorFunction() {
+
+    cityTextContent = this.textContent
+    console.log(cityTextContent)
+
+    //Clear Prior Data
+    currDayList.textContent = ""
+    fiveDayList.textContent = ""
+
+    //API Current URL
+    var currFullRequestURL = currRequestURL + cityTextContent + apiKey
+    console.log(currFullRequestURL)
+
+    //API Forecast URL
+    var fullRequestURL = requestURL + cityTextContent + apiKey
+    console.log(fullRequestURL)
+
+    //API Forecast Fetch
+    fetch(fullRequestURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            //Temp
+            for (var i = 0; i < 40; i = i + 8) {
+                var dateInfo = data.list[i].dt_txt;
+                var tempInfo = data.list[i].main.temp;
+                tempInfo = tempInfo - 273.15;
+                tempInfo = tempInfo * 1.8 + 32;
+                tempInfo = Math.round(tempInfo);
+                var windInfo = data.list[i].wind.speed;
+                var humidityInfo = data.list[i].main.humidity;
+                var cloudInfo = data.list[i].weather[0].main;
+                console.log(cloudInfo);
+
+                if (cloudInfo == "Thunderstorm") {
+
+                    var weatherInfo = " 🌩 " + "  Date: " + dateInfo + " | Temp: " + tempInfo + " °F " + " | Wind: " + windInfo + " MPH " + " | Humidity: " + humidityInfo + " %";
+                    var tempItem = document.createElement("li");
+                    tempItem.textContent = weatherInfo;
+                    fiveDayList.appendChild(tempItem);
+
+                }
+
+                else if (cloudInfo == "Clouds") {
+
+                    var weatherInfo = " ⛅ " + "  Date: " + dateInfo + " | Temp: " + tempInfo + " °F " + " | Wind: " + windInfo + " MPH " + " | Humidity: " + humidityInfo + " %";
+                    var tempItem = document.createElement("li");
+                    tempItem.textContent = weatherInfo;
+                    fiveDayList.appendChild(tempItem);
+                }
+
+                else if (cloudInfo == "Mist" || cloudInfo == "Rain") {
+
+                    var weatherInfo = " 🌧 " + "  Date: " + dateInfo + " | Temp: " + tempInfo + " °F " + " | Wind: " + windInfo + " MPH " + " | Humidity: " + humidityInfo + " %";
+                    var tempItem = document.createElement("li");
+                    tempItem.textContent = weatherInfo;
+                    fiveDayList.appendChild(tempItem);
+                }
+
+                else {
+
+                    var weatherInfo = " 🌞 " + "  Date: " + dateInfo + " | Temp: " + tempInfo + " °F " + " | Wind: " + windInfo + " MPH " + " | Humidity: " + humidityInfo + " %";
+                    var tempItem = document.createElement("li");
+                    tempItem.textContent = weatherInfo;
+                    fiveDayList.appendChild(tempItem);
+                }
+
+            }
+        })
+
+    //API Current URL
+    fetch(currFullRequestURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            var currentDate = new Date(data.dt * 1000);
+            var cityName = data.name;
+            var currTempInfo = data.main.temp;
+            var currWindInfo = data.wind.speed;
+            var currHumidityInfo = data.main.humidity;
+            var latInfo = data.coord.lat;
+            var longInfo = data.coord.lon;
+            var fullUVRequestURL = uvRequestURL + "lat=" + latInfo + "&lon=" + longInfo + "&exclude=hourly,daily&appid=e7ddda053e014bb0464681d224273dcf"
+            console.log(fullUVRequestURL);
+            currTempInfo = currTempInfo - 273.15;
+            currTempInfo = currTempInfo * 1.8 + 32;
+            currTempInfo = Math.round(currTempInfo);
+            var cloudCurrent = data.weather[0].main;
+            console.log(cloudCurrent);
+
+            if (cloudCurrent == "Thunderstorm") {
+
+                var currWeatherInfo = " 🌩 " + cityName + " | " + currentDate + " | Temp: " + currTempInfo + " °F " + " | Wind: " + currWindInfo + " MPH " + " | Humidity: " + currHumidityInfo + " %";
+                var currTempItem = document.createElement("li");
+                currTempItem.textContent = currWeatherInfo;
+                currDayList.appendChild(currTempItem);
+
+            }
+
+            else if (cloudCurrent == "Clouds") {
+
+                var currWeatherInfo = " ⛅ " + cityName + " | " + currentDate + " | Temp: " + currTempInfo + " °F " + " | Wind: " + currWindInfo + " MPH " + " | Humidity: " + currHumidityInfo + " %";
+                var currTempItem = document.createElement("li");
+                currTempItem.textContent = currWeatherInfo;
+                currDayList.appendChild(currTempItem);
+
+            }
+
+            else if (cloudCurrent == "Mist" || cloudCurrent == "Rain") {
+
+                var currWeatherInfo = " 🌧 " + cityName + " | " + currentDate + " | Temp: " + currTempInfo + " °F " + " | Wind: " + currWindInfo + " MPH " + " | Humidity: " + currHumidityInfo + " %";
+                var currTempItem = document.createElement("li");
+                currTempItem.textContent = currWeatherInfo;
+                currDayList.appendChild(currTempItem);
+
+            }
+
+            else {
+
+                var currWeatherInfo = " 🌞 " + cityName + " | " + currentDate + " | Temp: " + currTempInfo + " °F " + " | Wind: " + currWindInfo + " MPH " + " | Humidity: " + currHumidityInfo + " %";
+                var currTempItem = document.createElement("li");
+                currTempItem.textContent = currWeatherInfo;
+                currDayList.appendChild(currTempItem);
+
+            }
+
+            //UV Index Pull
+            fetch(fullUVRequestURL)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    var currUVItem = document.createElement("li");
+                    if (data.current.uvi <= 2) {
+                        currUVItem.textContent = "UV Index: " + data.current.uvi + " 🟢​ "
+                    }
+
+                    else if (data.current.uvi <= 5) {
+                        currUVItem.textContent = "UV Index: " + data.current.uvi + " 🟡​ "
+                    }
+
+                    else {
+                        currUVItem.textContent = "UV Index: " + data.current.uvi + " ​🔴​ "
+                    }
+                    currDayList.appendChild(currUVItem);
                 })
         })
 
-});
-
-
-console.log(typeof priorCity);
-console.log(typeof searchButton);
-
-//City Prior Search
-priorCity.addEventListener("click", function () {
-    console.log("hello world")
-    currentDayForecast.textContent = "";
-    fiveDayForecast.textContent = "";
-    fiveDayList.textContent = "";
-    currDayList.textContent = "";
-});
+};
